da1f8e6119c45e0d0b5f58f669b16f199f9467b2
Julio Uribe <uribejulioc@gmail.com>
2023-06-15 14:06:01 -0700

    Finish phase 1 and 2 for largest_contigous_subsum

--- problems.rb
+++ problems.rb
@@ -1,14 +1,15 @@
 
+require "byebug"
 # def my_min1(array)
 #     array.each do |ele1|
-#       smallest = true 
+#       smallest = true
 #         array.each do |ele2|
 #            if ele1 > ele2
 #              smallest = false
 #            end
 #        end
-#        if smallest 
-#         return ele1 
+#        if smallest
+#         return ele1
 #        end
 #     end
 
@@ -17,8 +18,8 @@
 
 # def my_min(array)
 #     min = array[0]
-#     array.each do |ele| 
-#         min = ele if ele < min 
+#     array.each do |ele|
+#         min = ele if ele < min
 #     end
 #     min
 # end
@@ -29,7 +30,7 @@
 
 # def largest_contiguous_subsum1(arr)
 #     sub_sets = []
-#     (0...arr.length).each do |start| 
+#     (0...arr.length).each do |start|
 #         (start...arr.length).each do |stop|
 #             sub_sets << arr[start..stop]
 #         end
@@ -38,17 +39,41 @@
 # end
 
 def largest_contiguous_subsum(arr)
-    largest = arr[0]
-    (0...arr.length).each do |start| 
-        (start...arr.length).each do |stop|
-            current_sum = arr[start..stop].sum
-             largest = current_sum if current_sum > largest
+    # Handle case when numbers are all negative.
+    all_negative = true
+    largest_num = arr[0]
+    arr.each do |x|
+        if x > largest_num
+            largest_num = x
+        end
+        if x >= 0
+            all_negative = false
+            break
         end
     end
-    largest
+    if all_negative
+        return largest_num
+    end
+
+    # Loop again adding up consecutive values
+    largest_sum = arr[0]
+    current_sum = arr[0]
+    (1...arr.length).each do |idx|          # 0, 0
+        current_sum += arr[idx]                  #
+        if current_sum > largest_sum        #
+            largest_sum = current_sum       #
+        elsif arr[idx] < 0 and current_sum < 0
+            current_sum = 0                 #
+        end
+    end
+    largest_sum
 end
 
 
 list = [2, 3, -6, 7, -6, 7]
+list2 = [5, 3, -7]
+list3 = [-5, -1, -3]
 
 p largest_contiguous_subsum(list) # => 8
+p largest_contiguous_subsum(list2) # => 8
+p largest_contiguous_subsum(list3) # => -1
